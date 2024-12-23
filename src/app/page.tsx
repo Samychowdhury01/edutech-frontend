a1c7f32153b65b3c56396014437055e342bd659a
samychowdhury01 <samychowdhury84@gmail.com>
2024-12-23 11:16:11 +0600

    initial frontend project setup done with nextjs, tailwind CSS and Shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import HeroSectionContainer from "@/components/home/hero-section-container";
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeroSectionContainer/>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/home/hero-section-container.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroSectionContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HeroSectionContainer;
